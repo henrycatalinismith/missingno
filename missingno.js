dc7585244cb844c50cd1134192cdae8b62ddc6a6
Henry Catalini Smith <henry@catalinismith.se>
2017-05-21 20:33:45 +0200

    Re-run on every document change

--- missingno.js
+++ missingno.js
@@ -1,11 +1,28 @@
-const selector = '.tweet .content .AdaptiveMedia img';
-const images = document.querySelectorAll(selector)
+const addAnotations = () => {
+  const selector = '.tweet .content .AdaptiveMedia img';
+  const images = document.querySelectorAll(selector)
 
-Array.from(images).map(image => {
-  const div = document.createElement('div');
-  div.classList.add('missingno-annotation');
-  div.innerHTML = image.alt;
-  div.title = image.alt;
+  Array.from(images)
+    .filter(image => !image.getAttribute('data-missingno-annotated'))
+    .map(image => {
+      const div = document.createElement('div');
+      div.classList.add('missingno-annotation');
+      div.innerHTML = image.alt;
+      div.title = image.alt;
 
-  image.parentElement.appendChild(div);
-})
+      image.setAttribute('data-missingno-annotated', true);
+      image.parentElement.appendChild(div);
+    });
+}
+
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(() => addAnotations());
+});
+
+const config = {
+  attributes: true,
+  childList: true,
+  characterData: false
+};
+
+observer.observe(document.body, config);
